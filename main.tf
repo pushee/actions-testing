9831d20e53d74ead11ab89a09b7929106ba119b1
Mike Fiala <michael.fiala@qut.edu.au>
2024-02-13 00:28:55 +1000

    oops...

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.32"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "sheepu-tfstate"
+    region  = "ap-southeast-2"
+    key     = "test/actions-testing/terraform.tfstate"
+  }
+  
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "ap-southeast-2"
+  profile = "sheepu.admin"
+}
+
+resource "aws_instance" "action_instance" {
+  ami  = "ami-02eec49345a878486" #  AMZN Linux 2
+  instance_type = "t2.micro"     # Free-tier eligible instance type
+}
